842d34970bc4548e0d91c6d283b393dbbeda7aea
IWantYou <yykaras411@outlook.com>
2017-06-01 09:41:08 +0800

    insert books

---
+++ bookorama/insert_book.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Book-O-Rama Book Entry Results</title>
+</head>
+<body>
+<h1>Book-O-Rama Book Entry Results</h1>
+<?php
+	//create short variable names
+	$isbn=$_POST['isbn'];
+	$author=$_POST['author'];
+	$title=$_POST['title'];
+	$price=$_POST['price'];
+
+	if (!$isbn||!$author||!$title||!$price) {
+		echo "You have not entered all the required details.<br/>"."Please go back and try again.";
+		exit;
+	}
+
+	if (!get_magic_quotes_gpc()) {
+		$isbn=addslashes($isbn);  //格式化字符串
+		$author=addslashes($author);
+		$title=addslashes($title);
+		$price=doubleval($price);
+	}
+
+	@ $db= new mysqli('localhost','root','ytzx1234','books');
+
+	if(mysqli_connect_errno()){
+		echo "Error: Could not connect to database. Please try again later.";
+		exit;
+	}
+	$query="insert into books values ('".$isbn."','".$author."','".$title."','".$price."')";
+	$result = $db->query($query);
+
+	if($result){
+		echo $db->affected_rows." book insert into database.";
+	}else{
+		echo "An error has occured. The item was not added.";
+	}
+
+	$db->close();
+?>
+</body>
+</html>
